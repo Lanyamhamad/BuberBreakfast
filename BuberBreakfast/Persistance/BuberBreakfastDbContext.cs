07bc0b6dcf67e0f7c5c13fcdc6bed88d8849223c
lanya <lanyamhamad02@gmail.com>
2024-09-24 23:21:04 -0700

    add Migrations

--- BuberBreakfast/Persistance/BuberBreakfastDbContext.cs
+++ BuberBreakfast/Persistance/BuberBreakfastDbContext.cs
@@ -12,7 +12,11 @@ public class BuberBreakfastDbContext : DbContext
         
     }
 
-    public DbSet<Breakfast> Breakfasts { get; set; }
+    public DbSet<Breakfast> Breakfasts { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BuberBreakfastDbContext).Assembly);    
+    }
 
 
 }
